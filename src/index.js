66a037e33d57de6668b1494319ed8bb55285df98
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-11 16:37:03 +0100

    Add to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import noteToFrequency from './noteToFrequency';
-
 import Interval from './constants/Interval';
 import Mode from './constants/Mode';
 import Scale from './constants/Scale';
 import Chord from './constants/Chord';
 
+import noteToFrequency from './noteToFrequency';
+
+import isHemitonicScale from './isHemitonicScale';
+import isDiatonicScale from './isDiatonicScale';
+import isMode from './isMode';
+import isPentatonicScale from './isPentatonicScale';
+import isHeptatonicScale from './isHeptatonicScale';
+
+import isTriad from './isTriad';
+
 export default {
-  noteToFrequency,
-  Scale,
   Interval,
   Mode,
-  Chord
+  Scale,
+  Chord,
+
+  noteToFrequency,
+
+  isHemitonicScale,
+  isDiatonicScale,
+  isMode,
+  isPentatonicScale,
+  isHeptatonicScale,
+
+  isTriad
 };
