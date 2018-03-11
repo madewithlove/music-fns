189de1feaf1c293f43ec2fdf894546b83d70682b
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-11 16:14:45 +0100

    Added Pentatonic scales

---
+++ src/constants/Scale/MAJOR_PENTATONIC.js
@@ -0,0 +1,6 @@
+// https://en.wikipedia.org/wiki/Chromatic_scale
+
+import { T } from '../Interval/Short';
+import { A2 } from '../Interval/AugmentedDiminished';
+
+export default [T, T, A2, T, A2];
---
+++ src/constants/Scale/MINOR_PENTATONIC.js
@@ -0,0 +1,6 @@
+// https://en.wikipedia.org/wiki/Chromatic_scale
+
+import { T } from '../Interval/Short';
+import { A2 } from '../Interval/AugmentedDiminished';
+
+export default [A2, T, T, A2, T];
