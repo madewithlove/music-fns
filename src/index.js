6ddad080d6441fe43455f39000d4068095fd4f8d
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-11 15:47:00 +0100

    Add chord to index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import noteToFrequency from './noteToFrequency';
 import Interval from './constants/Interval';
 import Mode from './constants/Mode';
 import Scale from './constants/Scale';
+import Chord from './constants/Chord';
 
 export default {
   noteToFrequency,
   Scale,
   Interval,
-  Mode
+  Mode,
+  Chord
 };
