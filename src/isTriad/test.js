ef970675ece0fb13ef77d736f86944f56a475649
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-11 15:47:09 +0100

    Add isTriad helper

---
+++ src/isTriad/test.js
@@ -0,0 +1,33 @@
+import isTriad from './';
+
+describe('isTriad', () => {
+  it('should return true on Major Triad', () => {
+    const pitches = [0, 4, 7];
+    expect(isTriad(pitches)).toBe(true);
+  });
+
+  it('should return true on Minor Triad', () => {
+    const pitches = [0, 3, 7];
+    expect(isTriad(pitches)).toBe(true);
+  });
+
+  it('should return true on Diminished Triad', () => {
+    const pitches = [0, 3, 6];
+    expect(isTriad(pitches)).toBe(true);
+  });
+
+  it('should return true on Augmented Triad', () => {
+    const pitches = [0, 4, 8];
+    expect(isTriad(pitches)).toBe(true);
+  });
+
+  it('should return false when input is not a triad', () => {
+    const pitches = [0, 3, 8];
+    expect(isTriad(pitches)).toBe(false);
+  });
+
+  it('should return false when input is not a triad', () => {
+    const pitches = [0, 3, 7, 3];
+    expect(isTriad(pitches)).toBe(false);
+  });
+});
