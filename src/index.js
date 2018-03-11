45a2e5f4d0fc797816a5da66f0509e05c2af0ed2
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-11 12:35:00 +0100

    Added constants to global

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import noteToFrequency from './noteToFrequency';
 
+import Interval from './constants/Interval';
+import Mode from './constants/Mode';
+import Scale from './constants/Scale';
+
 export default {
-  noteToFrequency
+  noteToFrequency,
+  Scale,
+  Interval,
+  Mode
 };
