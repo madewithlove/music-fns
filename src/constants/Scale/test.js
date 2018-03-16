487203f04f440cb0024ddd13ec7e8e19326dd30a
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-16 14:50:52 +0100

    Reorder scales

--- src/constants/Scale/test.js
+++ src/constants/Scale/test.js
@@ -1,19 +1,64 @@
+import ALTERED from './ALTERED';
+import BLUES from './BLUES';
 import CHROMATIC from './CHROMATIC';
+import ENIGMATIC from './ENIGMATIC';
 import HARMONIC_MINOR from './HARMONIC_MINOR';
-import NATURAL_MINOR from './NATURAL_MINOR';
-import MAJOR from './MAJOR';
-
 import MAJOR_PENTATONIC from './MAJOR_PENTATONIC';
+import MAJOR from './MAJOR';
+import MELODIC_MINOR from './MELODIC_MINOR';
 import MINOR_PENTATONIC from './MINOR_PENTATONIC';
+import NATURAL_MINOR from './NATURAL_MINOR';
+import PROMETHEUS from './PROMETHEUS';
+import WHOLE_TONE from './WHOLE_TONE';
 
 import AEOLIAN from '../Mode/AEOLIAN';
 import IONIAN from '../Mode/IONIAN';
+
+describe('Altered Scale', () => {
+  const pitches = [1, 2, 1, 2, 2, 2, 2];
+
+  it(`should return ${pitches}`, () => {
+    expect(ALTERED).toEqual(pitches);
+  });
+});
+
+describe('Blues Scale', () => {
+  const pitches = [3, 2, 1, 1, 3, 2];
+
+  it(`should return ${pitches}`, () => {
+    expect(BLUES).toEqual(pitches);
+  });
+});
 
 describe('Chromatic Scale', () => {
   const scale = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
   it(`should return ${scale}`, () => {
     expect(CHROMATIC).toEqual(scale);
+  });
+});
+
+describe('Enigmatic Scale', () => {
+  const pitches = [1, 3, 2, 2, 2, 1, 1];
+
+  it(`should return ${pitches}`, () => {
+    expect(ENIGMATIC).toEqual(pitches);
+  });
+});
+
+describe('Harmonic Minor Scale', () => {
+  const scale = [2, 1, 2, 2, 1, 3, 1];
+
+  it(`should return ${scale}`, () => {
+    expect(HARMONIC_MINOR).toEqual(scale);
+  });
+});
+
+describe('Major Pentatonic Scale', () => {
+  const scale = [2, 2, 3, 2, 3];
+
+  it(`should return ${scale}`, () => {
+    expect(MAJOR_PENTATONIC).toEqual(scale);
   });
 });
 
@@ -29,11 +74,19 @@ describe('Major Scale', () => {
   });
 });
 
-describe('Harmonic Minor Scale', () => {
-  const scale = [2, 1, 2, 2, 1, 3, 1];
+describe('Melodic Minor Scale', () => {
+  const scale = [2, 1, 2, 2, 2, 2, 1];
 
   it(`should return ${scale}`, () => {
-    expect(HARMONIC_MINOR).toEqual(scale);
+    expect(MELODIC_MINOR).toEqual(scale);
+  });
+});
+
+describe('Minor Pentatonic Scale', () => {
+  const pitches = [3, 2, 2, 3, 2];
+
+  it(`should return ${pitches}`, () => {
+    expect(MINOR_PENTATONIC).toEqual(pitches);
   });
 });
 
@@ -49,18 +102,18 @@ describe('Natural Minor Scale', () => {
   });
 });
 
-describe('Major Pentatonic Chord', () => {
-  const scale = [2, 2, 3, 2, 3];
+describe('Prometheus Scale', () => {
+  const pitches = [2, 2, 2, 3, 1, 2];
 
-  it(`should return ${scale}`, () => {
-    expect(MAJOR_PENTATONIC).toEqual(scale);
+  it(`should return ${pitches}`, () => {
+    expect(PROMETHEUS).toEqual(pitches);
   });
 });
 
-describe('Minor Pentatonic Chord', () => {
-  const pitches = [3, 2, 2, 3, 2];
+describe('Whole Tone Scale', () => {
+  const pitches = [2, 2, 2, 2, 2, 2];
 
   it(`should return ${pitches}`, () => {
-    expect(MINOR_PENTATONIC).toEqual(pitches);
+    expect(WHOLE_TONE).toEqual(pitches);
   });
 });
