b3d5610c5226a84cf6acf4406ea4450cf095cccb
Sambego <sambellen@gmail.com>
2018-03-16 18:33:54 +0100

    Fix typos and check equal objects

--- src/noteToObject/test.js
+++ src/noteToObject/test.js
@@ -3,41 +3,47 @@ import parseNote from './';
 describe('parseNote', () => {
   it('should return an object with note information on when it is a valid note', () => {
     const note = 'A';
-    const result = parseNote(note);
 
-    expect(result.note).toBe('A');
+    expect(parseNote(note)).toEqual({
+      note: 'A',
+      accidental: undefined,
+      octave: undefined
+    });
   });
 
   it('should return an object with note and accidental information on when it is a valid note with accidental', () => {
     const note = 'A#';
-    const result = parseNote(note);
 
-    expect(result.note).toBe('A');
-    expect(result.accidental).toBe('#');
+    expect(parseNote(note)).toEqual({
+      note: 'A',
+      accidental: '#',
+      octave: undefined
+    });
   });
 
-  it('should return an object with note,accidental and ocatave information on when it is a valid note with accidental and octave', () => {
+  it('should return an object with note,accidental and octave information on when it is a valid note with accidental and octave', () => {
     const note = 'A#2';
-    const result = parseNote(note);
 
-    expect(result.note).toBe('A');
-    expect(result.accidental).toBe('#');
-    expect(result.octave).toBe(2);
+    expect(parseNote(note)).toEqual({
+      note: 'A',
+      accidental: '#',
+      octave: 2
+    });
   });
 
-  it('should return an object with note and ocatave information on when it is a valid note with octave', () => {
+  it('should return an object with note and octave information on when it is a valid note with octave', () => {
     const note = 'A2';
-    const result = parseNote(note);
 
-    expect(result.note).toBe('A');
-    expect(result.accidental).toBe(undefined);
-    expect(result.octave).toBe(2);
+    expect(parseNote(note)).toEqual({
+      note: 'A',
+      accidental: undefined,
+      octave: 2
+    });
   });
 
-  it('should return false if the note is not valid', () => {
+  it('should throw an error if the note is not valid', () => {
     const note = 'H';
-    const result = parseNote(note);
 
-    expect(result).toBe(false);
+    expect(() => parseNote(note)).toThrow();
   });
 });
