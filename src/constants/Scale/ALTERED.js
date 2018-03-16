9a0a34c9f69028bc84a55294d9e6d1dbb8cde21a
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-16 14:50:44 +0100

    Add scales

---
+++ src/constants/Scale/ALTERED.js
@@ -0,0 +1,5 @@
+// https://en.wikipedia.org/wiki/Altered_scale
+
+import { S, T } from '../Interval/Short';
+
+export default [S, T, S, T, T, T, T];
---
+++ src/constants/Scale/WHOLE_TONE.js
@@ -0,0 +1,5 @@
+// https://en.wikipedia.org/wiki/Whole_tone_scale
+
+import { T } from '../Interval/Short';
+
+export default Array.from({ length: 6 }, () => T);
