2709c61090fdc2be2b04717a3ac87adbc655165d
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-17 20:34:51 +0100

    Prettier

--- src/noteToObject/test.js
+++ src/noteToObject/test.js
@@ -44,6 +44,8 @@ describe('noteToObject', () => {
   it('should throw an error if the note is not valid', () => {
     const note = 'H';
 
-    expect(() => noteToObject(note)).toThrow(`"H" is not a valid scientific note.`);
+    expect(() => noteToObject(note)).toThrow(
+      `"H" is not a valid scientific note.`
+    );
   });
 });
