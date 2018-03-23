e4ce7fdfcef714fdc06efc307b29d7c5c09837f3
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-23 23:44:40 +0100

    Add accidental constants

--- src/constants/Accidental.js
+++ src/constants/Accidental.js
@@ -4,10 +4,21 @@ export const FLAT_LETTER = 'b';
 export const SHARP_SYMBOL = '♯';
 export const SHARP_LETTER = '#';
 
+export const LETTER = 'LETTER';
+export const SYMBOL = 'SYMBOL';
+
+export const SHARP = 'SHARP';
+export const FLAT = 'FLAT';
+
 export default {
   FLAT_SYMBOL,
   FLAT_LETTER,
 
   SHARP_SYMBOL,
-  SHARP_LETTER
+  SHARP_LETTER,
+
+  FLAT,
+  SHARP,
+  LETTER,
+  SYMBOL
 };
