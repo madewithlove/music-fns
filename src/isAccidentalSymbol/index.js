2aa2749efe53ee3bc725e1327d0c05422d70d481
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-03-23 15:40:38 +0100

    Add isAccidentalSymbol

---
+++ src/isAccidentalSymbol/index.js
@@ -0,0 +1,13 @@
+// @flow
+
+import { FLAT_SYMBOL, SHARP_SYMBOL } from '../constants/Accidental';
+
+import noteToObject from '../noteToObject';
+
+const isAccidentalSymbol = (scientificNote: ScientificNote) => {
+  const { accidental } = noteToObject(scientificNote);
+
+  return accidental === FLAT_SYMBOL || accidental === SHARP_SYMBOL;
+};
+
+export default isAccidentalSymbol;
