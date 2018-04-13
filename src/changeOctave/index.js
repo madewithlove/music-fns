9b8c9a97f4f7f5d1596801b2885679fba740d840
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-04-13 17:00:23 +0200

    Remove null

--- src/changeOctave/index.js
+++ src/changeOctave/index.js
@@ -5,7 +5,7 @@ import objectToNote from '../objectToNote';
 
 const changeOctave = (
   scientificNote: ScientificNote,
-  octave: Octave = undefined
+  octave: Octave
 ): ScientificNote => {
   const noteObject = noteToObject(scientificNote);
   noteObject.octave = octave;
