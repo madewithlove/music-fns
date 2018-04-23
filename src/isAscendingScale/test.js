e6e42b891244d50362b3775155927baca5c20cc0
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-04-23 09:18:22 +0200

    Add isAscendingScale and isDescendingScale helpers

---
+++ src/isAscendingScale/test.js
@@ -0,0 +1,23 @@
+import isAscendingScale from './';
+
+describe('isAscendingScale', () => {
+  it('should return true on ascending scale', () => {
+    const scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+    expect(isAscendingScale(scale)).toBe(true);
+  });
+
+  it('should return false on descending scale', () => {
+    const scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+    expect(isAscendingScale(scale, { direction: -1 })).toBe(false);
+  });
+
+  it('should return true on ascending scale', () => {
+    const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1', 'C2'];
+    expect(isAscendingScale(scale)).toBe(true);
+  });
+
+  it('should return false on ascending scale', () => {
+    const scale = ['C2', 'B1', 'A1', 'G1', 'F1', 'E1', 'D1', 'C1'];
+    expect(isAscendingScale(scale)).toBe(false);
+  });
+});
---
+++ src/isDescendingScale/index.js
@@ -0,0 +1,21 @@
+// @flow
+
+import isValidScale from '../isValidScale';
+import getIntervals from '../getIntervals';
+
+type options = {
+  direction?: direction
+};
+
+const isAscendingScale = (scale: Scale, { direction = 1 }: options = {}) => {
+  if (direction === -1) return true;
+
+  if (!isValidScale(scale)) {
+    return false;
+  }
+
+  const intervals = getIntervals(scale, { direction });
+  return intervals.every(interval => interval < 0);
+};
+
+export default isAscendingScale;
