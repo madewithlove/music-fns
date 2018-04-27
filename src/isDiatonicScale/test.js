5b880d40394228027d63f5c5afbefce7f13dfeeb
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-04-27 16:11:40 +0200

    Uncomment tests

--- src/isDiatonicScale/test.js
+++ src/isDiatonicScale/test.js
@@ -3,53 +3,53 @@
 import isDiatonicScale from './';
 
 describe('isDiatonicScale', () => {
-  // it('should return true on diatonic scale', () => {
-  //   const scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
-  //   expect(isDiatonicScale(scale)).toBe(true);
-  // });
+  it('should return true on diatonic scale', () => {
+    const scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+    expect(isDiatonicScale(scale)).toBe(true);
+  });
 
-  // it('should return true on diatonic scale', () => {
-  //   const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1'];
-  //   expect(isDiatonicScale(scale)).toBe(true);
-  // });
+  it('should return true on diatonic scale', () => {
+    const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1'];
+    expect(isDiatonicScale(scale)).toBe(true);
+  });
 
-  // it('should return true on diatonic scale', () => {
-  //   const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1', 'C2'];
-  //   expect(isDiatonicScale(scale)).toBe(true);
-  // });
+  it('should return true on diatonic scale', () => {
+    const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1', 'C2'];
+    expect(isDiatonicScale(scale)).toBe(true);
+  });
 
   it('should return true on diatonic scale', () => {
     const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1'];
     expect(isDiatonicScale(scale, { direction: -1 })).toBe(true);
   });
 
-  // it('should return true on diatonic scale', () => {
-  //   const scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'];
-  //   expect(isDiatonicScale(scale)).toBe(true);
-  // });
+  it('should return true on diatonic scale', () => {
+    const scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'];
+    expect(isDiatonicScale(scale)).toBe(true);
+  });
 
-  // it('should return false on none diatonic scale', () => {
-  //   const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1', 'C1'];
-  //   expect(isDiatonicScale(scale)).toBe(false);
-  // });
+  it('should return false on none diatonic scale', () => {
+    const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1', 'C1'];
+    expect(isDiatonicScale(scale)).toBe(false);
+  });
 
-  // it('should return false on none diatonic scale', () => {
-  //   const scale = ['C', 'D', 'E', 'F', 'G', 'A#', 'B'];
-  //   expect(isDiatonicScale(scale)).toBe(false);
-  // });
+  it('should return false on none diatonic scale', () => {
+    const scale = ['C', 'D', 'E', 'F', 'G', 'A#', 'B'];
+    expect(isDiatonicScale(scale)).toBe(false);
+  });
 
-  // it('should return false on none diatonic scale', () => {
-  //   const scale = ['C', 'Fr', 'E'];
-  //   expect(isDiatonicScale(scale)).toBe(false);
-  // });
+  it('should return false on none diatonic scale', () => {
+    const scale = ['C', 'Fr', 'E'];
+    expect(isDiatonicScale(scale)).toBe(false);
+  });
 
-  // it('should return false on none diatonic scale', () => {
-  //   const scale = ['C', 'D', 'E2', 'F', 'G', 'A#', 'B'];
-  //   expect(isDiatonicScale(scale)).toBe(false);
-  // });
+  it('should return false on none diatonic scale', () => {
+    const scale = ['C', 'D', 'E2', 'F', 'G', 'A#', 'B'];
+    expect(isDiatonicScale(scale)).toBe(false);
+  });
 
-  // it('should return false on none diatonic scale', () => {
-  //   const scale = ['C1', 'D1', 'C1', 'F1', 'G1', 'A#1', 'B1'];
-  //   expect(isDiatonicScale(scale)).toBe(false);
-  // });
+  it('should return false on none diatonic scale', () => {
+    const scale = ['C1', 'D1', 'C1', 'F1', 'G1', 'A#1', 'B1'];
+    expect(isDiatonicScale(scale)).toBe(false);
+  });
 });
