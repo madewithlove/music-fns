a007f7b5c72d4d243ac27fcc7d090fc8d2d9661d
Jonas Drieghe <jonasd@madewithlove.be>
2018-11-14 07:04:52 +0100

    At test to prove bug in isDiatonicScale definition

--- src/isDiatonicScale/test.js
+++ src/isDiatonicScale/test.js
@@ -29,6 +29,11 @@ describe('isDiatonicScale', () => {
   });
 
   it('should return false on non-diatonic scale', () => {
+    const scale = ['C', 'D', 'E', 'F#', 'G#', 'A#', 'B'];
+    expect(isDiatonicScale(scale)).toBe(false);
+  });
+
+  it('should return false on non-diatonic scale', () => {
     const scale = ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1', 'C1'];
     expect(isDiatonicScale(scale)).toBe(false);
   });
