fbbaeee6cb3f7477d4ffc6cdaa5a63a6a5521182
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-12-27 00:35:26 +0100

    Update isFifth implementation

--- src/isFifth/index.js
+++ src/isFifth/index.js
@@ -2,6 +2,14 @@
 
 import { PERFECT_FIFTH } from '../constants/Interval/MinorMajor';
 
-const isFifth = (interval: Interval) => interval === PERFECT_FIFTH;
+import {
+  AUGMENTED_FIFTH,
+  DIMINISHED_FIFTH
+} from '../constants/Interval/AugmentedDiminished';
+
+const isFifth = (interval: Interval) =>
+  interval === PERFECT_FIFTH ||
+  interval === AUGMENTED_FIFTH ||
+  interval === DIMINISHED_FIFTH;
 
 export default isFifth;
--- src/isFifth/test.js
+++ src/isFifth/test.js
@@ -1,8 +1,16 @@
 import isFifth from './';
 
 describe('isFifth', () => {
-  it('should return true when interval is a fifth', () => {
+  it('should return true when interval is a perfect fifth', () => {
     expect(isFifth(7)).toBe(true);
+  });
+
+  it('should return true when interval is a diminished fifth', () => {
+    expect(isFifth(6)).toBe(true);
+  });
+
+  it('should return true when interval is a augmented fifth', () => {
+    expect(isFifth(8)).toBe(true);
   });
 
   it('should return false when interval is not a fifth', () => {
