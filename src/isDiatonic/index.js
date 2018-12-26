d7062eeeb17e22d318228c5ac9bdd0c96b4e0593
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2018-12-26 23:17:00 +0100

    Make failing isDiatonic test pass

--- src/isDiatonic/index.js
+++ src/isDiatonic/index.js
@@ -6,7 +6,8 @@ import isHeptatonic from '../isHeptatonic';
 import getIntervals from '../getIntervals';
 import normalize from '../normalize';
 
-import { SEMITONE, TONE } from '../constants/Interval/Names';
+import isSemitone from '../isSemitone';
+import isTone from '../isTone';
 
 type options = {
   direction?: direction
@@ -18,10 +19,16 @@ const isDiatonic = (scale: Scale, { direction = 1 }: options = {}) => {
   const normalizedScale = normalize(scale, { direction });
   const intervals = getIntervals(normalizedScale);
 
-  const semitones = intervals.filter(i => i === SEMITONE).length;
-  const tones = intervals.filter(i => i === TONE).length;
+  if (intervals.filter(isSemitone).length !== 2) return false;
+  if (intervals.filter(isTone).length !== 5) return false;
 
-  return semitones === 2 && tones === 5;
+  const [pos1, pos2] = intervals
+    .map((interval, position) => (isSemitone(interval) ? position : undefined))
+    .filter(position => position !== undefined);
+
+  if (pos2 - pos1 < 2) return false;
+
+  return true;
 };
 
 export default isDiatonic;
