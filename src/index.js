4e48853df846be5198c6bc2eaac9d5962b85c9e0
Chayut Ruksomya <chayut.ruksomya@nttdata.com>
2018-12-27 11:44:57 +0700

    Disable global-require, import/no-dynamic-require lint
    Because of one-shot manner importing, these ESlint rules must be disabled.

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ const ignoredFiles = ['constants', 'index.js'];
 
 const musicFunctions = {};
 
+/* eslint-disable  global-require, import/no-dynamic-require */
 fs
   .readdirSync(normalizedPath)
   .filter(file => !ignoredFiles.includes(file))
   .forEach(func => {
     musicFunctions[func] = require(`./${func}`);
   });
+/* eslint-enable */
 
 export default {
   Chord,
