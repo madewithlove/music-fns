610cacd8d4810750f767a9b0552d9b892febcefb
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2025-04-25 14:32:36 +0200

    better docs

--- src/getAccidental.ts
+++ src/getAccidental.ts
@@ -3,8 +3,8 @@ import { toObject } from "./toObject";
 /**
  * Retrieves the accidental from a note
  *
- * @param note - The note to extract the accidental from.
- * @returns The accidental character from the note, or undefined if no accidental is present.
+ * @param note - The note to extract the accidental (#, b) from.
+ * @returns The accidental character (#, b) from the note, or undefined if no accidental is present.
  */
 export function getAccidental(note: string) {
   const noteObject = toObject(note);
