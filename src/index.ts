aa06b80c8ed2077954e93b56bac29b0c14097dac
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2025-04-27 20:24:05 +0200

    simplify exports

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,32 @@
-import { isNote } from "./isNote";
-import { throwIfInvalidNote } from "./throwIfInvalidNote";
-import { toObject } from "./toObject";
-import { getRoot } from "./getRoot";
-import { getOctave } from "./getOctave";
-import { getAccidental } from "./getAccidental";
-import { hasAccidental } from "./hasAccidental";
-import { hasSharp } from "./hasSharp";
-import { hasFlat } from "./hasFlat";
-import { hasOctave } from "./hasOctave";
-
 declare const __brand: unique symbol;
 type Brand<T, B> = T & { [__brand]: B };
 
-type Flat = "b";
-type Sharp = "#";
-type Accidental = Flat | Sharp;
-type Octave = Brand<number, "Octave">;
-type Root = "C" | "D" | "E" | "F" | "G" | "A" | "B";
-type Frequency = Brand<number, "Frequency">;
-type ImpossibleNotes = `${"E" | "B"}${Sharp}` | `${"F" | "C"}${Flat}`;
-type NoteWithoutOctave =
+export * from "./isNote";
+export * from "./throwIfInvalidNote";
+export * from "./toObject";
+export * from "./getRoot";
+export * from "./getOctave";
+export * from "./getAccidental";
+export * from "./hasAccidental";
+export * from "./hasSharp";
+export * from "./hasFlat";
+export * from "./hasOctave";
+
+export type Flat = "b";
+export type Sharp = "#";
+export type Accidental = Flat | Sharp;
+export type Octave = Brand<number, "Octave">;
+export type Root = "C" | "D" | "E" | "F" | "G" | "A" | "B";
+export type Frequency = Brand<number, "Frequency">;
+export type ImpossibleNotes = `${"E" | "B"}${Sharp}` | `${"F" | "C"}${Flat}`;
+export type NoteWithoutOctave =
   | Root
   | Exclude<`${Root}${Accidental}`, ImpossibleNotes>;
-type NoteWithOctave = `${NoteWithoutOctave}${Octave}`;
-type Note = NoteWithoutOctave | NoteWithOctave;
+export type NoteWithOctave = `${NoteWithoutOctave}${Octave}`;
+export type Note = NoteWithoutOctave | NoteWithOctave;
 
-type NoteObject = {
+export type NoteObject = {
   root: Root;
   octave?: Octave;
   accidental?: Accidental;
-};
-
-export {
-  isNote,
-  throwIfInvalidNote,
-  toObject,
-  getRoot,
-  getOctave,
-  getAccidental,
-  hasAccidental,
-  hasSharp,
-  hasFlat,
-  hasOctave,
-};
-
-export type {
-  Sharp,
-  Flat,
-  Note,
-  Accidental,
-  Octave,
-  Root,
-  NoteObject,
-  NoteWithoutOctave,
-  NoteWithOctave,
-  Frequency,
 };
